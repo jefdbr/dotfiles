f789fef5f67dd511fb49865868f1cb170db68411
Jeffrey Debaere <jeffrey.debaere@ugent.be>
2026-02-28 22:40:16 +0100

    change script

--- scripts/collapse_workspaces.sh
+++ scripts/collapse_workspaces.sh
@@ -3,10 +3,12 @@
 for WS in $(hyprctl -j workspaces | jq 'sort_by(.monitorID) | .[].id'); do
   GAP_FOUND=0
   EXPECTED=1
+  TARGET=0
 
   for ID in $(hyprctl -j workspaces | jq '.[].id' | sort -n); do
     if [ "$ID" -ne "$EXPECTED" ]; then
       GAP_FOUND=1
+      TARGET=$EXPECTED
       break
     fi
 
@@ -21,7 +23,6 @@ for WS in $(hyprctl -j workspaces | jq 'sort_by(.monitorID) | .[].id'); do
     continue
   fi
 
-  TARGET=$(hyprctl -j workspaces | jq '.[].id' | sort -n | awk '$1 != ++i { print i; found=1; exit } END { if (!found) print i+1 }')
   for ADDR in $(hyprctl clients -j | jq -r ".[] | select(.workspace.id == $WS) | .address"); do
     hyprctl dispatch movetoworkspacesilent "$TARGET",address:$ADDR 2>/dev/null >/dev/null
   done
